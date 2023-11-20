957946ec44c371599e700ca1d29d993e749dedb0
CroseaMors <143801605+CroseaMors@users.noreply.github.com>
2023-11-20 11:33:37 +0800

    Add files via upload

--- src/AppBar.js
+++ src/AppBar.js
@@ -2,7 +2,9 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+import { Grid } from '@mui/material';
 import './App.css';
+import campus from './campus.jpg';
 
 export default function CenteredTabs() {
   const [value, setValue] = React.useState(0);
@@ -12,17 +14,28 @@ export default function CenteredTabs() {
   };
 
   return (
-    <Box sx={{ width: '90%', bgcolor: 'gold', marginTop: '10px', marginLeft: '5%', borderTopLeftRadius: '10px', borderTopRightRadius: '10px', borderBottomLeftRadius: '10px', borderBottomRightRadius: '10px'}}>
-      <Tabs value={value} onChange={handleChange} centered>
-        <Tab label="CREATE USER" />
-        <Tab label="SEARCH USER" />
-        <Tab label="PARKING SLOTS" />
-      </Tabs>
-    </Box>
-    
-  );
-  <Container maxWidth="sm">
-    
-  </Container>
-}
+    <div>
+      <Box sx={{ width: '90%', bgcolor: 'gold', marginTop: '10px', marginLeft: '5%', borderTopLeftRadius: '10px', borderTopRightRadius: '10px', borderBottomLeftRadius: '10px', borderBottomRightRadius: '10px'}}>
+        <Tabs value={value} onChange={handleChange} centered>
+          <Tab style={{marginRight: '100px', color: 'maroon', fontWeight: 'bolder'}} label="CREATE USER" />
+          <p style={{color: 'white', fontWeight: 'bolder'}}> | </p>
+          <Tab style={{marginRight: '100px', marginLeft: '100px', color: 'maroon', fontWeight: 'bolder'}} label="SEARCH USER" />
+          <p style={{color: 'white', fontWeight: 'bolder'}}> | </p>
+          <Tab style={{marginRight: '100px', marginLeft: '100px', color: 'maroon', fontWeight: 'bolder'}} label="PARKING SLOTS" />
+        </Tabs>
+      </Box>
 
+      {/* Single Editable Grid */}
+      <Grid container spacing={2} className="customGrid">
+        {/* Grid Item */}
+        <Grid item xs={12}>
+          {/* Your content for the grid item */}
+          <Box className="customBox">
+            {/* Content Goes Here */}
+            <img className="campus" src={campus} alt="Your Alt Text" />
+          </Box>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
